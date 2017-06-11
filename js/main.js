b1aa5a0a02a18519f87e42f6adf4e15dd0899d77
priyankamehta2690 <pmehta11@uncc.edu>
2017-06-11 00:51:46 -0400

    Homepage

---
+++ js/main.js
@@ -0,0 +1,84 @@
+jQuery(document).ready(function($){
+	var timelineBlocks = $('.cd-timeline-block'),
+		offset = 0.8;
+
+	//hide timeline blocks which are outside the viewport
+	hideBlocks(timelineBlocks, offset);
+
+	//on scolling, show/animate timeline blocks when enter the viewport
+	$(window).on('scroll', function(){
+		(!window.requestAnimationFrame)
+			? setTimeout(function(){ showBlocks(timelineBlocks, offset); }, 100)
+			: window.requestAnimationFrame(function(){ showBlocks(timelineBlocks, offset); });
+	});
+
+	function hideBlocks(blocks, offset) {
+		blocks.each(function(){
+			( $(this).offset().top > $(window).scrollTop()+$(window).height()*offset ) && $(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
+		});
+	}
+
+	function showBlocks(blocks, offset) {
+		blocks.each(function(){
+			( $(this).offset().top <= $(window).scrollTop()+$(window).height()*offset && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) && $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
+		});
+	}
+	
+	//open navigation clicking the menu icon
+	$('.cd-nav-trigger').on('click', function(event){
+		event.preventDefault();
+		toggleNav(true);
+	});
+	//close the navigation
+	$('.cd-close-nav, .cd-overlay').on('click', function(event){
+		event.preventDefault();
+		toggleNav(false);
+	});
+	//select a new section
+	$('.cd-nav li').on('click', function(event){
+		event.preventDefault();
+		var target = $(this),
+			//detect which section user has chosen
+			sectionTarget = target.data('menu');
+		if( !target.hasClass('cd-selected') ) {
+			//if user has selected a section different from the one alredy visible
+			//update the navigation -> assign the .cd-selected class to the selected item
+			target.addClass('cd-selected').siblings('.cd-selected').removeClass('cd-selected');
+			//load the new section
+			loadNewContent(sectionTarget);
+		} else {
+			// otherwise close navigation
+			toggleNav(false);
+		}
+	});
+
+	function toggleNav(bool) {
+		$('.cd-nav-container, .cd-overlay').toggleClass('is-visible', bool);
+		$('main').toggleClass('scale-down', bool);
+	}
+
+	function loadNewContent(newSection) {
+		//create a new section element and insert it into the DOM
+		var section = $('<section class="cd-section '+newSection+'"></section>').appendTo($('main'));
+		//load the new content from the proper html file
+		section.load(newSection+'.html .cd-section > *', function(event){
+			//add the .cd-selected to the new section element -> it will cover the old one
+			section.addClass('cd-selected').on('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+				//close navigation
+				toggleNav(false);
+			});
+			section.prev('.cd-selected').removeClass('cd-selected');
+		});
+
+		$('main').on('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+			//once the navigation is closed, remove the old section from the DOM
+			section.prev('.cd-section').remove();
+		});
+
+		if( $('.no-csstransitions').length > 0 ) {
+			//if browser doesn't support transitions - don't wait but close navigation and remove old item
+			toggleNav(false);
+			section.prev('.cd-section').remove();
+		}
+	}
+});
